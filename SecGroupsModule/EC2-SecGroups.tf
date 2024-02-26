d8f5f89517620d128d753e5b2aec734e053e4863
ben.shahar <benshahar99@gmail.com>
2024-02-26 14:22:17 +0200

    added security group module

---
+++ SecGroupsModule/EC2-SecGroups.tf
@@ -0,0 +1,46 @@
+resource "aws_security_group" "ec2_sg" {
+  name        = "allow_http/ssh"
+  description = "Allow ssh from local IP and inbound/outbound traffic"
+
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "terraform-ec2-security-group"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  tags = {
+    Name = "Terraform"
+  }
+}
