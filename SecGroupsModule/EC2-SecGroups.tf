c1a9b79b6364444eae619c1e8f26259cc9bf91fb
ben.shahar <benshahar99@gmail.com>
2024-02-26 15:51:32 +0200

    .

---
+++ SecGroupsModule/EC2-SecGroups.tf
@@ -0,0 +1,40 @@
+resource "aws_security_group" "terraform" {
+  name        = "allow_http/ssh"
+  description = "Allow ssh from local IP and inbound/outbound traffic"
+
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "terraform-ec2-security-group"
+  }
+}
+
+output "sg_id" {
+  value = aws_security_group.terraform.id
+}
