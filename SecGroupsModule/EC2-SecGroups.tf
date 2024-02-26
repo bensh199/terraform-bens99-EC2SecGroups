291b6b12f22fc87e4986aa2164dfa5d91a538954
ben.shahar <benshahar99@gmail.com>
2024-02-26 15:37:16 +0200

    changed module

--- SecGroupsModule/EC2-SecGroups.tf
+++ SecGroupsModule/EC2-SecGroups.tf
@@ -35,17 +35,6 @@ resource "aws_security_group" "ec2_sg" {
   }
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
-
-  tags = {
-    Name = "Terraform"
-  }
-}
-
 output "sg_id" {
-  description = "ID of project sg"
-  value       = aws_security_group.ec2_sg.id
+  value = aws_security_group.ec2_sg.id
 }
